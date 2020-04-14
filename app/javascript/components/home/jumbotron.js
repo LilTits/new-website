f7f7a047ac5987a49558a23db0174a54c1277bcd
LilTits <22703571+LilTits@users.noreply.github.com>
2020-04-14 11:18:18 +0200

    Styling the Jumbotron compoment

--- app/javascript/components/home/jumbotron.js
+++ app/javascript/components/home/jumbotron.js
@@ -1,15 +1,51 @@
 import React from "react";
+import styled from 'styled-components';
+
+const Section = styled.section`
+    background-color: #d74234;
+    min-height: 550px;
+    padding: 80px 0;
+    color: #fff;
+`
+
+const Header = styled.h1`
+    color: #fff;
+    font-weight:800;
+    font-size:40px;
+    line-height:52px;
+`
+
+const Subhead = styled.p`
+    font-size: 18px;
+    font-weight:500;
+`
+
+const Button = styled.a`
+    display: inline-block;
+    text-decoration: none;
+    font-weight: bold;
+    cursor: pointer;
+    border-radius: 0;
+    backgound: #fff;
+    padding: 10px 20px;
+    box-shadow: 0px 0px 0px 3px #473228,
+        -6px 6px #ef5f17,
+        -6px 6px 0px 3px #473228;
+`
 
 const Jumbotron = () => {
     return(
-        <section>
+        <Section>
             <div className="home-section--1">
                 <div className="container">
                     <div className="row">
                         <div className="col col-sm-12 col-md-5">
                             <div className="pt-4 mt-4">
-                                <h1>React for Rails</h1>
-                                <p>Supercharge Ruby on Rails</p>
+                                <Header>I-Tea Development</Header>
+                                <Subhead>Surprise Motherf*cker</Subhead>
+                                <div className="cta-wrapper">
+                                    <Button className="btn btn-fancy">Discover More !</Button>
+                                </div>
                             </div>
                         </div>
                         <div className="col col-sm-12 col-md-7">
@@ -20,7 +56,7 @@ const Jumbotron = () => {
                     </div>
                 </div>
             </div>
-        </section>
+        </Section>
     )
 }
 
